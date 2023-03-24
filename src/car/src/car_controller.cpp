04ccc00fb56d52e13a8645eaa4068512223f6048
Brian Erickson <berickson@gmail.com>
2023-03-23 22:04:16 -0700

    simplified

--- src/car/src/car_controller.cpp
+++ src/car/src/car_controller.cpp
@@ -1,7 +1,6 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/int32.hpp"
 #include "car_msgs/msg/update.hpp"
 using std::placeholders::_1;
 
@@ -11,36 +10,16 @@ class CarControllerNode : public rclcpp::Node
     CarControllerNode()
     : Node("car_controller")
     {
-      
-      subscription_ = this->create_subscription<std_msgs::msg::Int32>(
-      "micro_ros_platformio_node_publisher", rclcpp::SensorDataQoS(), std::bind(&CarControllerNode::topic_callback, this, _1));
       car_update_subscription_ = this->create_subscription<car_msgs::msg::Update>(
-      "car_update", rclcpp::SensorDataQoS(), std::bind(&CarControllerNode::car_update_topic_callback, this, _1));
+      "car/update", rclcpp::SensorDataQoS(), std::bind(&CarControllerNode::car_update_topic_callback, this, _1));
     }
 
   private:
     void car_update_topic_callback(const car_msgs::msg::Update::SharedPtr msg) const
     {
-      static int i = 0;
-
-      if (i % 100  == 0) {
-        RCLCPP_INFO(this->get_logger(), "car_update callback #%d ms: '%d'",i, msg->ms);
-      }
-      ++i;
-
+        RCLCPP_INFO_ONCE(this->get_logger(), "Running and receiving car update messages");
     }
 
-    void topic_callback(const std_msgs::msg::Int32::SharedPtr msg) const
-    {
-      static int i = 0;
-
-      if (i % 100  == 0) {
-        RCLCPP_INFO(this->get_logger(), "callback #%d I heard: '%d'",i, msg->data);
-      }
-      ++i;
-
-    }
-    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr subscription_;
     rclcpp::Subscription<car_msgs::msg::Update>::SharedPtr car_update_subscription_;
 };
 
