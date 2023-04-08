c02d6dd91649cca6d4d86397faf88861bc68f18c
Brian Erickson <berickson@gmail.com>
2023-04-07 20:09:47 -0700

    can subscribe to topic without type

--- src/rosbridge_cpp/src/rosbridge_cpp.cpp
+++ src/rosbridge_cpp/src/rosbridge_cpp.cpp
@@ -22,22 +22,31 @@ class RosbridgeCppNode : public rclcpp::Node
       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
       timer_ = this->create_wall_timer(
       500ms, std::bind(&RosbridgeCppNode::timer_callback, this));
+      auto topics = this->get_topic_names_and_types();
+      topic_name_ = "/some_string";
 
-      generic_subscription_ = this->create_generic_subscription("car/update", "car_msgs/msg/Update", rclcpp::SensorDataQoS(), std::bind(&RosbridgeCppNode::generic_callback, this, _1));
-      RCLCPP_INFO(this->get_logger(), "Node done creating subsciption");
+      auto it_types = topics.find(topic_name_);
+      if(it_types != topics.end()) {
+        type_name_ = it_types->second[0];
+        generic_subscription_ = this->create_generic_subscription(topic_name_, type_name_, rclcpp::SensorDataQoS(), std::bind(&RosbridgeCppNode::generic_callback, this, _1));
+        RCLCPP_INFO(this->get_logger(), "Node done creating subsciption to %s %s", topic_name_.c_str(), type_name_.c_str());
+      } else {
+        RCLCPP_INFO(this->get_logger(), "Could not find topic");
+      }
+
+
     }
 
   private:
 
     void generic_callback(std::shared_ptr<rclcpp::SerializedMessage> serialized_msg) {
+      std::string message_type = "std_msgs/msg/String";
 
-
-      std::string message_identifier = "car/update";
-      std::string message_type = "car_msgs/msg/Update";
-
-
+      auto handle = generic_subscription_->get_message_type_support_handle();
+      RCLCPP_INFO(this->get_logger(), "typesupport identifier %s", handle.typesupport_identifier);
       static bool first_time = true;
       if(first_time) {
+        
         json_encoder_.set_message_type(message_type);
         first_time = false;
       }
@@ -53,6 +62,8 @@ class RosbridgeCppNode : public rclcpp::Node
       message.data = "Hello, world! " + std::to_string(count_++);
       publisher_->publish(message);
     }
+    std::string topic_name_;
+    std::string type_name_;
     rclcpp::TimerBase::SharedPtr timer_;
     JsonEncoder json_encoder_;    
     std::shared_ptr<rclcpp::GenericSubscription> generic_subscription_;
