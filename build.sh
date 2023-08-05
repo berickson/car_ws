48c52b0a43dbd525eaa7feee877a493aaf30d85e
Brian Erickson <berickson@gmail.com>
2023-08-04 19:55:25 -0700

    updates

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ colcon build \
         --symlink-install \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         -Wall -Wextra -Wpedantic
+
+colcon test --packages-select nav2_line_following_controller --event-handlers console_direct+
+
+# colcon test-result --verbose
