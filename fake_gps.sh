a68a2fe0a69091f7c9f73b8ab547abf75817f08f
Brian Erickson <berickson@gmail.com>
2024-04-06 16:01:20 -0700

    Basic ekf wired up, but not tuned

--- fake_gps.sh
+++ fake_gps.sh
@@ -3,6 +3,6 @@
 while true; do
   SEC=$(date +%s)
   NSEC=$(date +%N)
-  ros2 topic pub /gps/fix sensor_msgs/msg/NavSatFix "{header: {stamp: {sec: $SEC, nanosec: $NSEC}, frame_id: 'map'}, status: {status: 0, service: 1}, latitude: 33.770050, longitude: -118.193741, altitude: 0.0, position_covariance: [25.0, 0.0, 0.0, 0.0, 25.0, 0.0, 0.0, 0.0, 25.0], position_covariance_type: 1}" --once
+  ros2 topic pub /car/gps/fix sensor_msgs/msg/NavSatFix "{header: {stamp: {sec: $SEC, nanosec: $NSEC}, frame_id: 'map'}, status: {status: 0, service: 1}, latitude: 33.770050, longitude: -118.193741, altitude: 0.0, position_covariance: [25.0, 0.0, 0.0, 0.0, 25.0, 0.0, 0.0, 0.0, 25.0], position_covariance_type: 1}" --once
   sleep 0.1
 done
