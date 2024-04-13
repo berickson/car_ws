6f3e2c0a68b42c2d7fb71822c8e555d3c7a11920
Brian Erickson <berickson@gmail.com>
2024-04-12 21:13:56 -0700

    publish location and direction inside house

--- fake_car_gps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-  SEC=$(date +%s)
-  NSEC=$(date +%N)
-  ros2 topic pub /car/gps/fix sensor_msgs/msg/NavSatFix "{header: {stamp: {sec: $SEC, nanosec: $NSEC}, frame_id: 'map'}, status: {status: 0, service: 1}, latitude: 33.802169, longitude: -118.123311, altitude: 0.0, position_covariance: [0.001, 0.0, 0.0, 0.0, 0.001, 0.0, 0.0, 0.0, 0.001], position_covariance_type: 1}" --once
-  sleep 0.1
-done
--- fake_gps.sh
+++ fake_gps.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
+ros2 topic pub /car/gps/fix sensor_msgs/msg/NavSatFix "{header: {stamp: 'now', frame_id: 'base_link'}, status: {status: 0, service: 1}, latitude: 33.802178, longitude: -118.123382, altitude: 0.0, position_covariance: [1.0E-6, 0.0, 0.0, 0.0, 1.0E-6, 0.0, 0.0, 0.0, 1.0E-6], position_covariance_type: 1}" --times 5
 
-while true; do
-  SEC=$(date +%s)
-  NSEC=$(date +%N)
-  ros2 topic pub /gps/fix sensor_msgs/msg/NavSatFix "{header: {stamp: {sec: $SEC, nanosec: $NSEC}, frame_id: 'map'}, status: {status: 0, service: 1}, latitude: 33.770050, longitude: -118.193741, altitude: 0.0, position_covariance: [25.0, 0.0, 0.0, 0.0, 25.0, 0.0, 0.0, 0.0, 25.0], position_covariance_type: 1}" --once
-  sleep 0.1
-done
+ros2 topic pub /car/compass sensor_msgs/msg/Imu "{header: {stamp: 'now', frame_id: 'base_link'}, orientation: {z: 0.7071, w: -0.7071}, orientation_covariance: [1.0E-6, 0.0, 0.0, 0.0, 1.0E-6, 0.0, 0.0, 0.0, 1.0E-6]}" --times 5
