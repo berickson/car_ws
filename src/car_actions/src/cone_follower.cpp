f6b772d54392c7b17439f2ed9d7212c2e534e8bb
Brian Erickson <berickson@gmail.com>
2024-05-06 22:42:17 -0700

    Tune with time travel tf and lag calculations.

--- src/car_actions/src/cone_follower.cpp
+++ src/car_actions/src/cone_follower.cpp
@@ -41,6 +41,9 @@ public:
 
   // save the most recent scan
   sensor_msgs::msg::LaserScan::SharedPtr scan_msg_;
+  
+  // save the most recent marker
+  visualization_msgs::msg::Marker::SharedPtr goal_marker_;
 
 
   std::shared_ptr<GoalHandleFollowCone> goal_handle_;
@@ -80,17 +83,59 @@ public:
 
 
   void cone_marker_callback(const visualization_msgs::msg::MarkerArray::SharedPtr markers_msg) {
-    // if(goal_handle_.get() == nullptr) {
-    //   return;
-    // }
-
-    if(markers_msg->markers.size() <2) {
+    if(markers_msg->markers.size() < 2) {
       return;
     }
-    geometry_msgs::msg::PointStamped p1,p2;
-    auto & marker = markers_msg->markers[1];
+    goal_marker_ = std::make_shared<visualization_msgs::msg::Marker>(markers_msg->markers[1]);
+  }
 
-    auto t =tf_buffer_->lookupTransform("front_bumper", marker.header.frame_id, rclcpp::Time(0));
+  
+
+
+  void loop() {
+    if(goal_handle_.get() == nullptr) {
+      return;
+    }
+ 
+    // fail if we don't have messages
+    if(scan_msg_ == nullptr || goal_marker_ == nullptr) {
+      RCLCPP_WARN(this->get_logger(), "No valid scan or marker message");
+      goal_handle_->abort(std::make_shared<FollowCone::Result>());
+      goal_handle_ = nullptr;
+      return;
+    }
+
+    // calculate difference between current time and time of scan
+    auto scan_time_diff = this->now() - scan_msg_->header.stamp;
+    auto marker_time_diff = this->now() - goal_marker_->header.stamp;
+    if(scan_time_diff > rclcpp::Duration(std::chrono::seconds(1)) || marker_time_diff > rclcpp::Duration(std::chrono::seconds(1))) {
+      goal_handle_->abort(std::make_shared<FollowCone::Result>());
+      goal_handle_ = nullptr;
+      RCLCPP_INFO(this->get_logger(), "Scan or marker message is too old");
+      return;
+    }
+
+    geometry_msgs::msg::PointStamped p1,p2;
+    auto & marker = *goal_marker_;
+
+
+
+    // find transform from where the bumber is now to where the cone was when it was detected
+    rclcpp::Time now = this->get_clock()->now();
+    rclcpp::Time when = marker.header.stamp;
+    auto to_frame = "front_bumper";
+    auto from_frame = marker.header.frame_id;
+    auto fixed_frame = "odom";
+    auto t = tf_buffer_->lookupTransform(
+        to_frame,
+        now,
+        from_frame,
+        when,
+        fixed_frame,
+        std::chrono::milliseconds(50)
+    );
+
+
 
     p1.header = marker.header;
     p1.point = marker.pose.position;
@@ -117,9 +162,10 @@ public:
     get_parameter<double>("goal_distance", stop_distance);
     double distance_remaining = distance - stop_distance;
 
+    double lag_time = 0.3; // expected system lag in velocity PID
     // velocity to stop in time
     float velocity = distance_remaining > 0 ?
-      std::clamp(sqrt(2 * max_accel * distance_remaining), min_velocity, max_velocity) 
+      std::clamp(sqrt(2 * max_accel * distance_remaining)-lag_time*max_accel, min_velocity, max_velocity) 
       : 0;
 
 
@@ -142,100 +188,14 @@ public:
         goal_handle_ = nullptr;
       }
     }
-    
-    
   }
 
-  
-
-
-
-  void cone_detection_callback(const vision_msgs::msg::Detection2DArray::SharedPtr detections_msg) {
-    return;
-    if(goal_handle_.get() == nullptr) {
-      return;
-    }
-    for(auto detection : detections_msg->detections) {
-      float x_angle_degrees = (x_resolution/2.0 - detection.bbox.center.position.x)/x_resolution * x_fov_degrees;
-      float x_width_degrees = detection.bbox.size_x * x_fov_degrees / x_resolution;
-      float width_radians = x_width_degrees * (M_PI / 180.0); 
-      float cone_width = 0.38; // meters, width of the cone
-      float cone_distance = (cone_width/2.0) / tan(width_radians/2.0);
-
-      // find scan line in center of cone and print distance
-      float scan_distance = std::numeric_limits<float>::infinity();
-      int min_scan_index = -1;
-
-      // we'll just look at the center of the scan
-      int center_count = 20;
-
-      if(scan_msg_ != nullptr) {
-        for(int i = scan_msg_->ranges.size() - center_count; i < scan_msg_->ranges.size(); i++) {
-          if(scan_msg_->ranges[i] < scan_distance) {
-            scan_distance = scan_msg_->ranges[i];
-            min_scan_index = i;
-          }
-        }
-       for(int i = 0; i < center_count; i++) {
-          if(scan_msg_->ranges[i] < scan_distance) {
-            scan_distance = scan_msg_->ranges[i];
-            min_scan_index = i;
-          }
-        }
-        RCLCPP_INFO(this->get_logger(), "scan distance: %3.2f  index: %d", scan_distance, min_scan_index);
-      }
-
-      // adjust distances to be from front of the car
-      cone_distance -= 0.17; // distance from camera to front of car
-      scan_distance -= 0.29; // distance from camera to front of car
-
-      float distance = (cone_distance < 1 && scan_distance < 1) ? scan_distance : cone_distance;
-
-      double max_velocity;
-      get_parameter<double>("max_velocity", max_velocity);
-      double max_accel;
-      get_parameter<double>("accel", max_accel);
-      double min_velocity;
-      get_parameter<double>("min_velocity", min_velocity);
-      double stop_distance; // distance from front of car to cone to stop at
-      get_parameter<double>("goal_distance", stop_distance);
-      double distance_remaining = distance - stop_distance;
-
-      // velocity to stop in time
-      float velocity = distance_remaining > 0 ?
-        std::clamp(sqrt(2 * max_accel * distance_remaining), min_velocity, max_velocity) 
-        : 0;
-
-      RCLCPP_INFO(this->get_logger(), "detection degrees: %3.1f width_degrees: %3.1f distance: %3.2f vel: %3.2f", x_angle_degrees, x_width_degrees, distance, velocity);
-
-      geometry_msgs::msg::Twist cmd_vel_msg;
-      cmd_vel_msg.linear.x = velocity;
-      if(velocity > 0) {
-        float correction_distance = std::clamp(distance_remaining / 2, 0.5, 2.0); // distance to complete turn to currect angle
-        cmd_vel_msg.angular.z = x_angle_degrees * (M_PI / 180.0) *  cmd_vel_msg.linear.x / correction_distance;
-      } else {
-        cmd_vel_msg.angular.z = 0;
-      }
-
-
-
-      cmd_vel_publisher_->publish(cmd_vel_msg);
-      if(distance_remaining <= 0) {
-        if(goal_handle_.get() != nullptr) {
-          RCLCPP_INFO(this->get_logger(), "Goal reached, completing action");
-          goal_handle_->succeed(std::make_shared<FollowCone::Result>());
-          goal_handle_ = nullptr;
-        }
-      }
-
-      break; // we'll just grab the first detection (if any)
-    }
-  }
+  // timer member variable
+  rclcpp::TimerBase::SharedPtr timer_;
 
 
   cone_follower_node() : Node("cone_follower_node") {
     RCLCPP_INFO(this->get_logger(), "Cone Follower Node has started");
-    detection_subscription_ = this->create_subscription<vision_msgs::msg::Detection2DArray>("car/oakd/color/cone_detections", 1, std::bind(&cone_follower_node::cone_detection_callback, this, std::placeholders::_1));
     marker_subscription_ = this->create_subscription<visualization_msgs::msg::MarkerArray>("car/oakd/color/cone_markers", 1, std::bind(&cone_follower_node::cone_marker_callback, this, std::placeholders::_1));
     scan_subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>("scan", 1, [this](const sensor_msgs::msg::LaserScan::SharedPtr msg) {
       scan_msg_ = msg;
@@ -283,9 +243,11 @@ public:
         this->declare_parameter("min_velocity",0.1, param_desc);
       }
 
-
-            double min_velocity = 0.1;
-      double stop_distance = 0.3; // distance from front of car to cone to stop at
+      // create timer for main loop
+      const int timer_hz = 100;
+      timer_ = this->create_wall_timer(std::chrono::milliseconds(10), [this]() {
+        loop();
+      });
 
     }
 };
